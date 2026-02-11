dee50f7f59e7bf6a7e21269491d3f66e72e22f3a
Velazquez David <d.velazquez.g757@edu.utc.com>
2026-02-11 08:41:53 -0600

    despliegue

---
+++ src/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app  = express();
+const morgan = require('morgan'); // Muestra las solicitudes
+const cors = require('cors'); // Permite conexiones externas al servidor
+
+// Configuración del sevidor en el puerto 3000
+app.set('port', process.env.PORT || 3000);
+app.set('json spaces', 2); // Habilitamos JSON
+
+app.use(morgan('dev')); // Cargar morgan maneja errores
+
+app.use(express.urlencoded({extended:false}));
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req,res)=> {
+    res.json(
+        {
+            "Title":"Kiubo brother"
+        }
+    )
+});
+
+app.post('/sumar',(req, res) =>{
+    const{num1, num2} = req.body; 
+    
+         if (!num1 || !num2 ){
+            return res.status(400).send({error: 'Faltan números por sumar'})
+         }
+         const resultado = num1 + num2;
+         res.send({resultado});
+});
+
+
+
+app.listen(app.get('port'), () => {
+    console.log("Servidor en puerto 3000")
+});
+
+
+
+// app.use(express.urlencoded({extended:false}));
